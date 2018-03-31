29a85aaefa69dd982f192505d446df86da2eeaf0
shubhojit <royshubhojit@gmail.com>
2018-04-01 08:21:28 +1000

    Files modifed as per start of mobile first design.

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('watch', function() {
+    browserSync.init({
+        notify: false,
+        server: {                       
+            baseDir: "app"          
+        }
+    });
+    
+    watch('./app/assets/styles/**/*.css', function() {
+        gulp.start('styles');
+    });
+    
+    watch('./app/index.html', function() {
+        browserSync.reload();
+    });
+});
+
+gulp.task('cssInject', ['styles'], function() {
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
+});
